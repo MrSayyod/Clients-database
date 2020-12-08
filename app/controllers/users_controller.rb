f08ba86f1cee9990989e936a6a0f7ce93b11367e
MrSayyod <ssayyod@gmail.com>
2020-12-08 19:20:21 +0500

    Create 'edit, 'update' and 'destroy' methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,5 +20,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
 end
